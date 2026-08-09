4afa07d5d708efbc940bc15a16105252ebd92287
Yug Rathod <yugrathod1025@gmail.com>
2026-08-09 14:54:25 +0530

    Time: 6 ms (99.69%), Space: 48.5 MB (6.65%) - LeetHub

--- 0875-koko-eating-bananas/0875-koko-eating-bananas.java
+++ 0875-koko-eating-bananas/0875-koko-eating-bananas.java
@@ -17,18 +17,16 @@ class Solution {
             }
         }
         int s = 1;
-        int ans = -1;
         int e = max;
         while ( s <= e){
             int mid = s + ( e - s)/2;
             if (isValidAns(piles, h, mid)){
-                ans = mid;
                 e = mid - 1;
             }
             else{
                 s = mid + 1; 
             }
         }
-        return ans;
+        return s;
     }
 }
