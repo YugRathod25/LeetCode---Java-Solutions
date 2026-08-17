22d14d06f38f756bc8e6015ae05a867f1dc686a2
Yug Rathod <yugrathod1025@gmail.com>
2026-08-17 23:15:19 +0530

    Time: 0 ms (100%), Space: 48.4 MB (50.67%) - LeetHub

---
+++ 0704-binary-search/0704-binary-search.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int search(int[] nums, int target) {
+        int s = 0;
+        int n = nums.length;
+        int e = n - 1;
+        while(s <= e){
+            int mid = s + (e - s)/2;
+            if(nums[mid] == target){
+                return mid;
+            }
+            else if(nums[mid] > target){
+                e = mid - 1;
+            }
+            else{
+                s = mid + 1;
+            }
+        }
+        return -1;
+
+    }
+}
